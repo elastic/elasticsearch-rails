2b260994bd2ad2f82db6baea8a82152175e1c78f
Karel Minarik <karmi@karmi.cz>
2013-11-07 21:22:17 +0100

    [MODEL] Added a Mongoid adapter for `Elasticsearch::Model::Response::Records` and refactored adapter implementation
    Example:
    --------
    
        response.records.class
        => Elasticsearch::Model::Response::Records
    
        response.records.records.class
        => Mongoid::Criteria
    
        > response.records.records.inspect
        => #<Mongoid::Criteria
          selector: {"_id"=>{"$in"=>["3", "1"]}}
          options:  {}
          class:    Article
          embedded: false>
    
        > response.records.to_a
        => [#<Article _id: 3, id: nil, title: "Foo Foo", published_at: nil>,
         #<Article _id: 1, id: nil, title: "Foo", published_at: nil>]
    
        > response.records.asc(:title).to_a
        => [#<Article _id: 1, id: nil, title: "Foo", published_at: nil>,
         #<Article _id: 3, id: nil, title: "Foo Foo", published_at: nil>]

--- elasticsearch-model/lib/elasticsearch/model.rb
+++ elasticsearch-model/lib/elasticsearch/model.rb
@@ -5,7 +5,9 @@ require 'elasticsearch'
 require 'hashie'
 
 require 'elasticsearch/model/adapter'
+require 'elasticsearch/model/adapters/default'
 require 'elasticsearch/model/adapters/active_record'
+require 'elasticsearch/model/adapters/mongoid'
 
 require 'elasticsearch/model/response'
 require 'elasticsearch/model/response/base'
---
+++ elasticsearch-model/lib/elasticsearch/model/adapters/mongoid.rb
@@ -0,0 +1,40 @@
+module Elasticsearch
+  module Model
+    module Adapter
+      module Mongoid
+
+        module Records
+
+          # Return the `ActiveRecord::Relation` instance
+          #
+          def records
+            criteria = klass.where(:id.in => @ids)
+
+            criteria.instance_exec(response['hits']['hits']) do |hits|
+              define_singleton_method :to_a do
+                self.entries.sort_by { |e| hits.index { |hit| hit['_id'] == e.id.to_s } }
+              end
+            end
+
+            criteria
+          end
+
+          # Intercept call to sorting methods, so we can ignore the order from Elasticsearch
+          #
+          %w| asc desc order_by |.each do |name|
+            define_method name do |*args|
+              criteria = records.__send__ name, *args
+              criteria.instance_exec do
+                define_singleton_method(:to_a) { self.entries }
+              end
+
+              criteria
+            end
+          end
+
+        end
+      end
+
+    end
+  end
+end
