e0f2d3b578e94b9bdb744f55bbad3a55471c6f43
Karel Minarik <karel.minarik@elasticsearch.com>
2013-11-08 21:44:18 +0100

    [MODEL] Allow using Hash, JSON string or simple keyword searches in `MyModel.search`

--- elasticsearch-model/lib/elasticsearch/model/searching.rb
+++ elasticsearch-model/lib/elasticsearch/model/searching.rb
@@ -5,7 +5,19 @@ module Elasticsearch
       module ClassMethods
 
         def search(query_or_payload, options={})
-          response = client.search index: index_name, type: document_type, q: query_or_payload
+          case
+            # search query: ...
+            when query_or_payload.respond_to?(:to_hash)
+              response = client.search index: index_name, type: document_type, body: query_or_payload.to_hash
+
+            # search '{ "query" : ... }'
+            when query_or_payload.is_a?(String) && query_or_payload =~ /^\s*{/
+              response = client.search index: index_name, type: document_type, body: query_or_payload
+
+            # search '...'
+            else
+              response = client.search index: index_name, type: document_type, q: query_or_payload
+          end
           Response::Response.new(self, response)
         end
 
