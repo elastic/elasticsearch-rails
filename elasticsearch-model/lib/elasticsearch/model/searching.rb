f16c0c12ed73e738933ade7a42718b07d5b5782c
Karel Minarik <karel.minarik@elasticsearch.com>
2013-11-08 21:33:07 +0100

    [MODEL] Added `MyModel.index_name` and `MyModel.document_type`

--- elasticsearch-model/lib/elasticsearch/model/searching.rb
+++ elasticsearch-model/lib/elasticsearch/model/searching.rb
@@ -5,7 +5,7 @@ module Elasticsearch
       module ClassMethods
 
         def search(query_or_payload, options={})
-          response = client.search index: self.model_name.collection, q: query_or_payload
+          response = client.search index: index_name, type: document_type, q: query_or_payload
           Response::Response.new(self, response)
         end
 
