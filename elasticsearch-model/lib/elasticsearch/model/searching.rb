005c649689653795a4c3ac400a8168111a5650aa
Karel Minarik <karmi@karmi.cz>
2013-11-08 22:12:17 +0100

    [MODEL] Allow passing index name and document type in `MyModel.search`
    Example:
    --------
    
        Article.search('foo', index: 'my-special-index')
        Article.search('foo', index: 'my-special-index', type: 'my-type')

--- elasticsearch-model/lib/elasticsearch/model/searching.rb
+++ elasticsearch-model/lib/elasticsearch/model/searching.rb
@@ -5,19 +5,23 @@ module Elasticsearch
       module ClassMethods
 
         def search(query_or_payload, options={})
+          __index_name    = options[:index] || index_name
+          __document_type = options[:type]  || document_type
+
           case
             # search query: ...
             when query_or_payload.respond_to?(:to_hash)
-              response = client.search index: index_name, type: document_type, body: query_or_payload.to_hash
+              response = client.search index: __index_name, type: __document_type, body: query_or_payload.to_hash
 
             # search '{ "query" : ... }'
             when query_or_payload.is_a?(String) && query_or_payload =~ /^\s*{/
-              response = client.search index: index_name, type: document_type, body: query_or_payload
+              response = client.search index: __index_name, type: __document_type, body: query_or_payload
 
             # search '...'
             else
-              response = client.search index: index_name, type: document_type, q: query_or_payload
+              response = client.search index: __index_name, type: __document_type, q: query_or_payload
           end
+
           Response::Response.new(self, response)
         end
 
