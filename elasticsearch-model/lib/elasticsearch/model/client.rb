7a20d23c630896e652ba92f8e3c2b81a34a721fd
Karel Minarik <karmi@karmi.cz>
2013-11-11 11:48:57 +0100

    [MODEL] Added `MyModel#client` method to get/set Elasticsearch client in the instance

--- elasticsearch-model/lib/elasticsearch/model/client.rb
+++ elasticsearch-model/lib/elasticsearch/model/client.rb
@@ -11,6 +11,15 @@ module Elasticsearch
         end
       end
 
+      module InstanceMethods
+
+        # Get or set the client for a specific record
+        #
+        def client client=nil
+          @client = client || @client || self.class.client
+        end
+      end
+
     end
   end
 end
