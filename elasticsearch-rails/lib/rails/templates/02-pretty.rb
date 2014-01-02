9746c984a0f7df3c7ae645e29be680d0406cb24f
Karel Minarik <karel.minarik@elasticsearch.com>
2014-01-02 12:46:01 +0100

    [RAILS] Updated and polished the "basic" and "pretty" application templates

--- elasticsearch-rails/lib/rails/templates/02-pretty.rb
+++ elasticsearch-rails/lib/rails/templates/02-pretty.rb
@@ -2,86 +2,45 @@
 
 # (See: 01-basic.rb)
 
-# ----- Add loading Bootstrap assets --------------------------------------------------------------
+puts
+say_status  "README", "Updating Readme...\n", :yellow
+puts        '-'*80, ''; sleep 0.25
+
+append_to_file 'README.rdoc', <<-README
+
+== [2] Pretty
+
+The `pretty` template builds on the `basic` version and brings couple of improvements:
+
+* Using the [Bootstrap](http://getbootstrap.com) framework to enhance the visual style of the application
+* Using an `Article.search` class method to customize the default search definition
+* Highlighting matching phrases in search results
+* Paginating results with Kaminari
+
+README
+
+git add:    "README.rdoc"
+git commit: "-m '[02] Updated the application README'"
+
+# ----- Add gems into Gemfile ---------------------------------------------------------------------
 
 puts
-say_status  "Bootstrap", "Adding Bootstrap asset links into the 'application' layout...\n", :yellow
-puts        '-'*80, ''; sleep 0.5
+say_status  "Rubygems", "Adding Rubygems into Gemfile...\n", :yellow
+puts        '-'*80, ''; sleep 0.25
 
-gsub_file 'app/views/layouts/application.html.erb', %r{<%= yield %>}, <<-CODE unless File.read('app/views/layouts/application.html.erb').include?('class="container"')
-<div class="container">
-<%= yield %>
-</div>
+# NOTE: Kaminari has to be loaded before Elasticsearch::Model so the callbacks are executed
+#
+insert_into_file 'Gemfile', <<-CODE, before: 'gem "elasticsearch"'
+
+# NOTE: Kaminari has to be loaded before Elasticsearch::Model so the callbacks are executed
+gem 'kaminari'
+
 CODE
 
-inject_into_file 'app/views/layouts/application.html.erb', <<-CODE, before: '</head>'
-  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
-CODE
+run "bundle install"
 
-git :commit => "-a -m 'Added loading Bootstrap assets in the application layout'"
-
-# ----- Customize the search form -----------------------------------------------------------------
-
-puts
-say_status  "Bootstrap", "Customizing the index page...\n", :yellow
-puts        '-'*80, ''; sleep 0.5
-
-gsub_file 'app/views/articles/index.html.erb', %r{<%= label_tag .* :search %>}m do |match|
-<<-CODE
-<div class="input-group">
-  <%= text_field_tag :q, params[:q], class: 'form-control', placeholder: 'Search...' %>
-
-  <span class="input-group-btn">
-    <button type="button" class="btn btn-default">
-      <span class="glyphicon glyphicon-search"></span>
-    </button>
-  </span>
-</div>
-CODE
-end
-
-git :commit => "-a -m 'Refactored the search form to use Bootstrap components'"
-
-# ----- Customize the results listing -------------------------------------------------------------
-
-gsub_file 'app/views/articles/index.html.erb', %r{<table>} do |match|
-  '<table class="table table-hover">'
-end
-
-gsub_file 'app/views/articles/index.html.erb', %r{<td><%= link_to [^%]+} do |match|
-  match.gsub!('<td>', '<td style="width: 50px">')
-  match.include?("btn") ? match : (match << ", class: 'btn btn-default btn-xs'")
-end
-
-gsub_file 'app/views/articles/index.html.erb', %r{<%= link_to ('New Article',\s*new_article_path|'All articles',\s*articles_path)} do |match|
-  match.include?("btn") ? match : (match += ", class: 'btn btn-primary btn-xs', style: 'color: #fff'")
-end
-
-git :commit => "-a -m 'Refactored the articles listing to use Bootstrap components'"
-
-puts
-say_status  "CSS", "Adding custom styles...\n", :yellow
-puts        '-'*80, ''; sleep 0.5
-
-append_to_file 'app/assets/stylesheets/application.css' do
-  unless File.read('app/assets/stylesheets/application.css').include?('.label-highlight')
-<<-CODE
-
-.label-highlight {
-  font-style: normal !important;
-  font-weight: normal !important;
-  font-size: 13px !important;
-  color: #000 !important;
-  background: #fff401;
-}
-CODE
-  else
-    ''
-  end
-end
-
-git :commit => "-a -m 'Added custom style definitions into application.css'"
+git add:    "Gemfile*"
+git commit: "-m 'Added the Kaminari gem'"
 
 # ----- Add `Article.search` class method ---------------------------------------------------------
 
@@ -89,7 +48,7 @@ puts
 say_status  "Model", "Adding a `Article.search` class method...\n", :yellow
 puts        '-'*80, ''; sleep 0.5
 
-inject_into_file 'app/models/article.rb', <<-CODE, after: 'include Elasticsearch::Model::Callbacks'
+insert_into_file 'app/models/article.rb', <<-CODE, after: 'include Elasticsearch::Model::Callbacks'
 
 
   def self.search(query)
@@ -114,8 +73,100 @@ inject_into_file 'app/models/article.rb', <<-CODE, after: 'include Elasticsearch
   end
 CODE
 
-git :add    => 'app/models/article.rb'
-git :commit => "-m 'Added a `Article.search` custom method'"
+gsub_file "test/models/article_test.rb", %r{# test "the truth" do.*?# end}m, <<-CODE
+
+  test "has a search method delegating to __elasticsearch__" do
+    Article.__elasticsearch__.expects(:search).with do |definition|
+      assert_equal 'foo', definition[:query][:multi_match][:query]
+    end
+
+    Article.search 'foo'
+  end
+CODE
+
+git add:    "app/models/article.rb"
+git add:    "test/models/article_test.rb"
+git commit: "-m 'Added an `Article.search` method'"
+
+# ----- Add loading Bootstrap assets --------------------------------------------------------------
+
+puts
+say_status  "Bootstrap", "Adding Bootstrap asset links into the 'application' layout...\n", :yellow
+puts        '-'*80, ''; sleep 0.5
+
+gsub_file 'app/views/layouts/application.html.erb', %r{<%= yield %>}, <<-CODE unless File.read('app/views/layouts/application.html.erb').include?('class="container"')
+<div class="container">
+<%= yield %>
+</div>
+CODE
+
+insert_into_file 'app/views/layouts/application.html.erb', <<-CODE, before: '</head>'
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+CODE
+
+git commit: "-a -m 'Added loading Bootstrap assets in the application layout'"
+
+# ----- Customize the search form -----------------------------------------------------------------
+
+puts
+say_status  "Bootstrap", "Customizing the index page...\n", :yellow
+puts        '-'*80, ''; sleep 0.5
+
+gsub_file 'app/views/articles/index.html.erb', %r{<%= label_tag .* :search %>}m do |match|
+<<-CODE
+<div class="input-group">
+  <%= text_field_tag :q, params[:q], class: 'form-control', placeholder: 'Search...' %>
+
+  <span class="input-group-btn">
+    <button type="button" class="btn btn-default">
+      <span class="glyphicon glyphicon-search"></span>
+    </button>
+  </span>
+</div>
+CODE
+end
+
+# ----- Customize the header -----------------------------------------------------------------
+
+gsub_file 'app/views/articles/index.html.erb', %r{<h1>Listing articles</h1>} do |match|
+  "<h1><%= controller.action_name == 'search' ? 'Searching articles' : 'Listing articles' %></h1>"
+end
+
+# ----- Customize the results listing -------------------------------------------------------------
+
+gsub_file 'app/views/articles/index.html.erb', %r{<table>} do |match|
+  '<table class="table table-hover">'
+end
+
+gsub_file 'app/views/articles/index.html.erb', %r{<td><%= link_to [^%]+} do |match|
+  match.gsub!('<td>', '<td style="width: 50px">')
+  match.include?("btn") ? match : (match + ", class: 'btn btn-default btn-xs'")
+end
+
+gsub_file 'app/views/articles/index.html.erb', %r{<br>\s*(<\%= link_to 'New Article'.*)}m do |content|
+  replace = content.match(%r{<br>\s*(<\%= link_to 'New Article'.*)}m)[1]
+  <<-END.gsub(/^  /, '')
+  <hr>
+
+  <p style="text-align: center; margin-bottom: 21px">
+    #{replace}
+  </p>
+  END
+end
+
+gsub_file 'app/views/articles/index.html.erb', %r{<%= link_to 'New Article',\s*new_article_path} do |match|
+  return match if match.include?('btn')
+  match + ", class: 'btn btn-primary btn-xs', style: 'color: #fff'"
+end
+
+gsub_file 'app/views/articles/index.html.erb', %r{<%= link_to 'All Articles',\s*articles_path} do |match|
+  return match if match.include?('btn')
+  "\n  " + match + ", class: 'btn btn-primary btn-xs', style: 'color: #fff'"
+end
+
+git add:    "app/views"
+git commit: "-m 'Refactored the articles listing to use Bootstrap components'"
 
 # ----- Use highlighted excerpts in the listing ---------------------------------------------------
 
@@ -131,8 +182,78 @@ gsub_file 'app/views/articles/index.html.erb', %r{<td><%= article.content %></td
   "<td><%= hit.try(:highlight).try(:content) ? hit.highlight.content.join('&hellip;').html_safe : article.content %></td>"
 end
 
-git :add    => '.'
-git :commit => "-m 'Improved the search results listing'"
+git commit: "-a -m 'Added highlighting for matches'"
+
+# ----- Paginate the results ----------------------------------------------------------------------
+
+gsub_file 'app/controllers/articles_controller.rb', %r{@articles = Article.all} do |match|
+  "@articles = Article.page(params[:page])"
+end
+
+gsub_file 'app/controllers/articles_controller.rb', %r{@articles = Article.search\(params\[\:q\]\).records} do |match|
+  "@articles = Article.search(params[:q]).page(params[:page]).records"
+end
+
+insert_into_file 'app/views/articles/index.html.erb', after: '</table>' do
+  <<-CODE.gsub(/^  /, '')
+
+
+  <div class="text-center">
+    <%= paginate @articles %>
+  </div>
+  CODE
+end
+
+generate "kaminari:views", "bootstrap", "--force"
+
+gsub_file 'app/views/kaminari/_paginator.html.erb', %r{<ul>}, '<ul class="pagination">'
+
+git add:    "."
+git commit: "-m 'Added pagination to articles listing'"
+
+# ----- Custom CSS --------------------------------------------------------------------------------
+
+puts
+say_status  "CSS", "Adding custom styles...\n", :yellow
+puts        '-'*80, ''; sleep 0.5
+
+append_to_file 'app/assets/stylesheets/application.css' do
+  unless File.read('app/assets/stylesheets/application.css').include?('.label-highlight')
+<<-CODE
+
+.label-highlight {
+  font-size: 100% !important;
+  font-weight: inherit !important;
+  font-style: inherit !important;
+  color: #333 !important;
+  background: #fff401 !important;
+}
+
+div.pagination {
+  text-align: center;
+  display: block;
+}
+
+div.pagination ul {
+  display: inline-block;
+}
+
+CODE
+  else
+    ''
+  end
+end
+
+git commit: "-a -m 'Added custom style definitions into application.css'"
+
+# ----- Generate 1,000 articles -------------------------------------------------------------------
+
+puts
+say_status  "Database", "Creating 1,000 articles...", :yellow
+puts        '-'*80, '';
+
+run  "rails runner 'Article.__elasticsearch__.create_index! force: true'"
+rake "db:seed COUNT=1_000"
 
 # ----- Print Git log -----------------------------------------------------------------------------
 
@@ -140,8 +261,8 @@ puts
 say_status  "Git", "Details about the application:", :yellow
 puts        '-'*80, ''
 
-git :tag => "pretty"
-git :log => "--reverse --oneline pretty...basic"
+git tag: "pretty"
+git log: "--reverse --oneline pretty...basic"
 
 # ----- Start the application ---------------------------------------------------------------------
 
