618b7f2f53271c520037567f851a13e0d5181c69
Karel Minarik <karel.minarik@elasticsearch.com>
2014-03-03 12:20:40 +0100

    Release 0.1.0

--- elasticsearch-model/lib/elasticsearch/model/version.rb
+++ elasticsearch-model/lib/elasticsearch/model/version.rb
@@ -1,5 +1,5 @@
 module Elasticsearch
   module Model
-    VERSION = "0.1.0.rc1"
+    VERSION = "0.1.0"
   end
 end
--- elasticsearch-rails/lib/elasticsearch/rails/version.rb
+++ elasticsearch-rails/lib/elasticsearch/rails/version.rb
@@ -1,5 +1,5 @@
 module Elasticsearch
   module Rails
-    VERSION = "0.1.0.rc1"
+    VERSION = "0.1.0"
   end
 end
