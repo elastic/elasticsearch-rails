c8548374f241b513e79f01059b43bf524fa5434b
Karel Minarik <karel.minarik@elasticsearch.com>
2014-03-25 19:22:30 +0100

    [STORE] Added the blank skeleton of the "elasticsearch-persistence" gem

---
+++ elasticsearch-persistence/lib/elasticsearch/persistence.rb
@@ -0,0 +1,7 @@
+require "elasticsearch/persistence/version"
+
+module Elasticsearch
+  module Persistence
+    # Your code goes here...
+  end
+end
---
+++ elasticsearch-persistence/lib/elasticsearch/persistence/version.rb
@@ -0,0 +1,5 @@
+module Elasticsearch
+  module Persistence
+    VERSION = "0.0.1"
+  end
+end
