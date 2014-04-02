c391a3b287dcca2556b6d67f34437c83315df568
Karel Minarik <karel.minarik@elasticsearch.com>
2014-04-02 10:34:01 +0200

    [STORE] Implemented the gateway pattern for the repository integration
    To provide flexibility to the end-user, the integration of the "Repository" module
    has been refactored to proxy all repository methods via a gateway.
    
    This allows users to set up the repository in a custom class, with class methods, in a DSL-like fashion:
    
        class NoteRepository
          include Elasticsearch::Persistence::Repository
    
          klass Note
          index :my_notes
    
          mapping do
            indexes :title, analyzer: 'snowball'
          end
    
          client.transport.logger = Logger.new(STDERR)
    
          gateway do
            def serialize(document)
              super.merge(special: 'stuff')
            end
          end
        end
    
    The bundled Repository class can be configured via a block passed to the initializer:
    
        repository = Elasticsearch::Persistence::Repository.new do
          klass Note
          index :my_notes
    
          mapping do
            indexes :title, analyzer: 'snowball'
          end
    
          client.transport.logger = Logger.new(STDERR)
        end
    
    The repository methods can be accessed via the class or instance methods:
    
        NoteRepository.klass Note
        repository.klass Note

--- elasticsearch-persistence/test/unit/repository_module_test.rb
+++ elasticsearch-persistence/test/unit/repository_module_test.rb
@@ -2,5 +2,111 @@ require 'test_helper'
 
 class Elasticsearch::Persistence::RepositoryModuleTest < Test::Unit::TestCase
   context "The repository module" do
+
+    class DummyModel
+      def initialize(attributes={})
+        @attributes = attributes
+      end
+
+      def to_hash
+        @attributes
+      end
+
+      def inspect
+        "<Note #{@attributes.inspect}>"
+      end
+    end
+
+    setup do
+      class DummyRepository
+        include Elasticsearch::Persistence::Repository
+      end
+    end
+
+    teardown do
+      Elasticsearch::Persistence::RepositoryModuleTest.__send__ :remove_const, :DummyRepository
+    end
+
+    context "when included" do
+      should "set up the gateway for the class and instance" do
+        assert_respond_to DummyRepository,     :gateway
+        assert_respond_to DummyRepository.new, :gateway
+
+        assert_instance_of Elasticsearch::Persistence::Repository::Class, DummyRepository.gateway
+        assert_instance_of Elasticsearch::Persistence::Repository::Class, DummyRepository.new.gateway
+      end
+
+      should "proxy repository methods from the class to the gateway" do
+        class DummyRepository
+          include Elasticsearch::Persistence::Repository
+
+          index :foobar
+          klass DummyModel
+          type  :my_dummy_model
+          mapping { indexes :title, analyzer: 'snowball' }
+        end
+
+        repository = DummyRepository.new
+
+        assert_equal :foobar,         DummyRepository.index
+        assert_equal DummyModel,      DummyRepository.klass
+        assert_equal :my_dummy_model, DummyRepository.type
+        assert_equal 'snowball', DummyRepository.mappings.to_hash[:my_dummy_model][:properties][:title][:analyzer]
+
+        assert_equal :foobar,         repository.index
+        assert_equal DummyModel,      repository.klass
+        assert_equal :my_dummy_model, repository.type
+        assert_equal 'snowball', repository.mappings.to_hash[:my_dummy_model][:properties][:title][:analyzer]
+      end
+
+      should "proxy repository methods from the instance to the gateway" do
+        class DummyRepository
+          include Elasticsearch::Persistence::Repository
+        end
+
+        repository = DummyRepository.new
+        repository.index :foobar
+        repository.klass DummyModel
+        repository.type  :my_dummy_model
+        repository.mapping { indexes :title, analyzer: 'snowball' }
+
+        assert_equal :foobar,         DummyRepository.index
+        assert_equal DummyModel,      DummyRepository.klass
+        assert_equal :my_dummy_model, DummyRepository.type
+        assert_equal 'snowball', DummyRepository.mappings.to_hash[:my_dummy_model][:properties][:title][:analyzer]
+
+        assert_equal :foobar,         repository.index
+        assert_equal DummyModel,      repository.klass
+        assert_equal :my_dummy_model, repository.type
+        assert_equal 'snowball', repository.mappings.to_hash[:my_dummy_model][:properties][:title][:analyzer]
+      end
+
+      should "allow to define gateway methods in the class definition" do
+        class DummyRepository
+          include Elasticsearch::Persistence::Repository
+
+          gateway do
+            def serialize(document)
+              'FAKE!'
+            end
+          end
+        end
+
+        repository = DummyRepository.new
+        repository.client.transport.logger = Logger.new(STDERR)
+
+        client = mock
+        client.expects(:index).with do |arguments|
+          assert_equal('xxx',   arguments[:id])
+          assert_equal('FAKE!', arguments[:body])
+        end
+        repository.gateway.expects(:client).returns(client)
+
+        repository.gateway.expects(:__get_id_from_document).returns('xxx')
+
+        repository.save( id: '123', foo: 'bar' )
+      end
+    end
+
   end
 end
