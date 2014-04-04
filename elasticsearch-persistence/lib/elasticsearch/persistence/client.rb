ca1270ddcfc3e1234612ea9459d08d0d7d6c3cc0
Karel Minarik <karel.minarik@elasticsearch.com>
2014-04-04 22:19:12 +0200

    [STORE] Added code annotation, documentation and examples

--- elasticsearch-persistence/lib/elasticsearch/persistence/client.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/client.rb
@@ -3,10 +3,40 @@ module Elasticsearch
     module Repository
 
       module Client
+
+        # Get or set the default client for this repository
+        #
+        # @example Set and configure the client for the repository class
+        #
+        #     class MyRepository
+        #       include Elasticsearch::Persistence::Repository
+        #       client Elasticsearch::Client.new host: 'http://localhost:9200', log: true
+        #     end
+        #
+        # @example Set and configure the client for this repository instance
+        #
+        #     repository.client Elasticsearch::Client.new host: 'http://localhost:9200', tracer: true
+        #
+        # @example Perform an API request through the client
+        #
+        #     MyRepository.client.cluster.health
+        #     repository.client.cluster.health
+        #     # => { "cluster_name" => "elasticsearch" ... }
+        #
         def client client=nil
           @client = client || @client || Elasticsearch::Persistence.client
         end
 
+        # Set the default client for this repository
+        #
+        # @example Set and configure the client for the repository class
+        #
+        #     MyRepository.client = Elasticsearch::Client.new host: 'http://localhost:9200', log: true
+        #
+        # @example Set and configure the client for this repository instance
+        #
+        #     repository.client = Elasticsearch::Client.new host: 'http://localhost:9200', tracer: true
+        #
         def client=(client)
           @client = client
           @client
--- elasticsearch-persistence/lib/elasticsearch/persistence/repository/class.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/repository/class.rb
@@ -2,6 +2,41 @@ module Elasticsearch
   module Persistence
     module Repository
 
+      # The default repository class, to be used either directly, or as a gateway in a custom repository class
+      #
+      # @example Standalone use
+      #
+      #     repository = Elasticsearch::Persistence::Repository::Class.new
+      #     # => #<Elasticsearch::Persistence::Repository::Class ...>
+      #     # > repository.save(my_object)
+      #     # => {"_index"=> ... }
+      #
+      #
+      # @example Shortcut use
+      #
+      #     repository = Elasticsearch::Persistence::Repository.new
+      #     # => #<Elasticsearch::Persistence::Repository::Class ...>
+      #
+      # @example Configuration via a block
+      #
+      #     repository = Elasticsearch::Persistence::Repository.new do
+      #       index 'my_notes'
+      #     end
+      #     # => #<Elasticsearch::Persistence::Repository::Class ...>
+      #     # > repository.save(my_object)
+      #     # => {"_index"=> ... }
+      #
+      # @example Accessing the gateway in a custom class
+      #
+      #     class MyRepository
+      #       include Elasticsearch::Persistence::Repository
+      #     end
+      #
+      #     repository = MyRepository.new
+      #
+      #     repository.gateway.client.info
+      #     => {"status"=>200, "name"=>"Venom", ... }
+      #
       class Class
         include Elasticsearch::Persistence::Repository::Client
         include Elasticsearch::Persistence::Repository::Naming
@@ -20,6 +55,10 @@ module Elasticsearch
           block.arity < 1 ? instance_eval(&block) : block.call(self) if block_given?
         end
 
+        # Return the "host" class, if this repository is a gateway hosted in another class
+        #
+        # @return [nil, Class]
+        #
         def host
           options[:host]
         end
