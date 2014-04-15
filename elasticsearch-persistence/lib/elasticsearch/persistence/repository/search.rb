afafa1239e7081013dffa592063dd3321cf22d85
Josef Pospíšil <josef.pospisil@laststar.eu>
2014-04-15 17:20:22 +0200

    [STORE] Fixed, that `search` respects `document_type` set in class
    Closes #77

--- elasticsearch-persistence/lib/elasticsearch/persistence/repository/search.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/repository/search.rb
@@ -40,7 +40,7 @@ module Elasticsearch
         # @return [Elasticsearch::Persistence::Repository::Response::Results]
         #
         def search(query_or_definition, options={})
-          type     = (klass ? __get_type_from_class(klass) : nil  )
+          type = document_type || (klass ? __get_type_from_class(klass) : nil  )
           case
           when query_or_definition.respond_to?(:to_hash)
             response = client.search( { index: index_name, type: type, body: query_or_definition.to_hash }.merge(options) )
