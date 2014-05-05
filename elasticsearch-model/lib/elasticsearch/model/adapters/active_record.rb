68bb8ad86125e5ef271971a0553a3ea17d27a85e
Russ Smith <russ@bashme.org>
2014-05-05 14:17:38 -0700

    Store the id_path for later use since ActiveRecord rejects it as a invalid option.

--- elasticsearch-model/lib/elasticsearch/model/adapters/active_record.rb
+++ elasticsearch-model/lib/elasticsearch/model/adapters/active_record.rb
@@ -91,10 +91,10 @@ module Elasticsearch
           # end
 
           def __find_in_batches(options={}, &block)
+            id_path = options.delete(id_path) || :id
             find_in_batches(options) do |batch|
-              options[:id_path] ||= :id
               batch_for_bulk = batch.map do |a|
-                { index: { _id: a.send(options[:id_path]), data: a.__elasticsearch__.as_indexed_json } }
+                { index: { _id: a.send(id_path), data: a.__elasticsearch__.as_indexed_json } }
               end
               yield batch_for_bulk
             end
