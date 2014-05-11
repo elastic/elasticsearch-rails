daf7b9c2b821d67a916a864fe8f79420abbaa122
Beat Jörg <beat.joerg@gmail.com>
2014-05-11 22:44:02 +0100

    [TEST] Added an integration test for as_indexed_json changed attribute miss because as_json returns symbols instead of strings in rails 3.2
    Related: #116

--- elasticsearch-model/test/integration/active_record_custom_serialization_test.rb
+++ elasticsearch-model/test/integration/active_record_custom_serialization_test.rb
@@ -56,6 +56,68 @@ module Elasticsearch
         end
       end
 
+
+      class ::ArticleWithCustomSerializationMethod < ActiveRecord::Base
+        include Elasticsearch::Model
+        include Elasticsearch::Model::Callbacks
+
+        mapping do
+          indexes :tag_list
+        end
+
+        def tag_list=(val)
+          attribute_will_change!('tag_list') unless val == @tag_list
+          @tag_list=val
+        end
+
+        def tag_list
+          @tag_list || 'foo bar'
+        end
+
+        def as_indexed_json(options={})
+          as_json(root: false, methods: [:tag_list])
+        end
+      end
+
+      context "ActiveRecord model with custom JSON serialization including method" do
+        setup do
+          ActiveRecord::Schema.define(:version => 1) do
+            create_table ArticleWithCustomSerializationMethod.table_name do |t|
+              t.string   :title
+            end
+          end
+
+          ArticleWithCustomSerializationMethod.delete_all
+          ArticleWithCustomSerializationMethod.__elasticsearch__.create_index! force: true
+        end
+
+        should "index the tag_list method when creating" do
+          ArticleWithCustomSerializationMethod.create! title: 'Test'
+
+          a = ArticleWithCustomSerializationMethod.__elasticsearch__.client.get \
+                index: 'article_with_custom_serialization_methods',
+                type:  'article_with_custom_serialization_method',
+                id:    '1'
+
+          assert_equal( { 'id'=> 1, 'title'=> 'Test', 'tag_list' => 'foo bar' }, a['_source'] )
+        end
+
+        should "index the updated tag_list attribute when updating" do
+          ArticleWithCustomSerializationMethod.create! title: 'Test'
+
+          article = ArticleWithCustomSerializationMethod.first
+          article.tag_list='UPDATED'
+          article.save!
+
+          a = ArticleWithCustomSerializationMethod.__elasticsearch__.client.get \
+                index: 'article_with_custom_serialization_methods',
+                type:  'article_with_custom_serialization_method',
+                id:    '1'
+
+          assert_equal( { 'id'=> 1, 'title'=> 'Test', 'tag_list' => 'UPDATED' }, a['_source'] )
+        end
+      end
+
     end
   end
 end
