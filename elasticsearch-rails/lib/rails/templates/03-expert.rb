0060718255fa4eff788ad98239b4f6b835b0b194
Matt <matt@ifttt.com>
2014-08-30 12:21:32 -0700

    [RAILS] Fixed incorrect filename in the 03-expert.rb application template
    Closes #223

--- elasticsearch-rails/lib/rails/templates/03-expert.rb
+++ elasticsearch-rails/lib/rails/templates/03-expert.rb
@@ -1,4 +1,4 @@
-#     $ rails new searchapp --skip --skip-bundle --template https://raw.github.com/elasticsearch/elasticsearch-rails/master/elasticsearch-rails/lib/rails/templates/03-complex.rb
+#     $ rails new searchapp --skip --skip-bundle --template https://raw.github.com/elasticsearch/elasticsearch-rails/master/elasticsearch-rails/lib/rails/templates/03-expert.rb
 
 # (See: 01-basic.rb, 02-pretty.rb)
 
