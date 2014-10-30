fa8b0bef518e034fb2ad7f5fa5308033aff77a07
Konstantin Zub <Zubkonst@gmail.com>
2014-10-30 04:36:06 -0700

    [RAILS] prevent double include alias method (:execute!)

--- elasticsearch-rails/lib/elasticsearch/rails/instrumentation/publishers.rb
+++ elasticsearch-rails/lib/elasticsearch/rails/instrumentation/publishers.rb
@@ -12,25 +12,23 @@ module Elasticsearch
 
           def self.included(base)
             base.class_eval do
-              alias_method :execute_without_instrumentation!, :execute!
-
-              def execute!
-                execute_with_instrumentation!
-              end
-
-              # Wrap `Search#execute!` and perform instrumentation
-              #
-              def execute_with_instrumentation!
-                ActiveSupport::Notifications.instrument "search.elasticsearch",
-                  name:   'Search',
-                  klass:  (self.klass.is_a?(Elasticsearch::Model::Proxy::ClassMethodsProxy) ? self.klass.target.to_s : self.klass.to_s),
-                  search: self.definition do
-                  execute_without_instrumentation!
-                end
+              unless method_defined?(:execute_without_instrumentation!)
+                alias_method :execute_without_instrumentation!, :execute!
+                alias_method :execute!, :execute_with_instrumentation!
               end
             end
           end
 
+          # Wrap `Search#execute!` and perform instrumentation
+          #
+          def execute_with_instrumentation!
+            ActiveSupport::Notifications.instrument "search.elasticsearch",
+              name:   'Search',
+              klass:  (self.klass.is_a?(Elasticsearch::Model::Proxy::ClassMethodsProxy) ? self.klass.target.to_s : self.klass.to_s),
+              search: self.definition do
+              execute_without_instrumentation!
+            end
+          end
         end
       end
     end
