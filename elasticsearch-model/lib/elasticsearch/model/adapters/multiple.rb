e4584d44f0f17c6bf6173adf3ab131a5db1fcc17
Miguel Fernandez <miguelfernandezfernandez@gmail.com>
2015-03-17 17:56:10 +0100

    Implement multi-model search

---
+++ elasticsearch-model/lib/elasticsearch/model/adapters/multiple.rb
@@ -0,0 +1,125 @@
+module Elasticsearch
+  module Model
+    module Adapter
+
+      # An adapter to be used for deserializing results from multiple models, retrieved through
+      # Elasticsearch::Model.search
+      #
+      # @see Elasticsearch::Model.search
+      #
+      module Multiple
+
+        Adapter.register self, lambda { |klass| klass.is_a? Multimodel }
+
+        module Records
+
+          # Returns an Array, which elements are the model instances represented
+          # by the search results.
+          #
+          # This means that if the models queried are a Mixture of ActiveRecord, Mongoid, or
+          # POROs, the elements contained in this array will also be instances of those models
+          #
+          # Ranking of results across multiple indexes is preserved, and queries made to the different
+          # model's datasources are minimal.
+          #
+          # Internally, it gets the results, as ranked by elasticsearch.
+          # Then results are grouped by _type
+          # Then the model corresponding to each _type is queried to retrieve the records
+          # Finally records are rearranged in the same way results were ranked.
+          #
+          # @return [ElasticSearch::Model]
+          #
+          def records
+            @_records ||= begin
+              result = []
+              by_type = __records_by_type
+              __hits.each do |hit|
+                result << by_type[__type(hit)][hit[:_id]]
+              end
+              result.compact
+            end
+          end
+
+          # Returns the record representation of the results retrieved from Elasticsearch, grouped
+          # by model type
+          #
+          # @example
+          # {Series  =>
+          #   {"1"=> #<Series id: 1, series_name: "The Who S01", created_at: "2015-02-23 17:18:28">},
+          #
+          # "Title =>
+          #   {"1"=> #<Title id: 1, name: "Who Strikes Back", created_at: "2015-02-23 17:18:28">}}
+          #
+          # @api private
+          #
+          def __records_by_type
+            array = __ids_by_type.map do |klass, ids|
+              records = __type_records(klass, ids)
+              ids = records.map(&:id).map(&:to_s)
+              [klass, Hash[ids.zip(records)]]
+            end
+            Hash[array]
+          end
+
+          # Returns the records for a specific type
+          #
+          # @api private
+          #
+          def __type_records(klass, ids)
+            if (adapter = Adapter.adapters[ActiveRecord]) && adapter.call(klass)
+              klass.where(klass.primary_key => ids)
+            elsif (adapter = Adapter.adapters[Mongoid]) && adapter.call(klass)
+              klass.where(:id.in => ids)
+            else
+              klass.find(ids)
+            end
+          end
+
+
+          # @return A Hash containing for each type, the ids to retrieve
+          #
+          # @example {Series =>["1"], Title =>["1", "5"]}
+          #
+          # @api private
+          #
+          def __ids_by_type
+            ids_by_type = {}
+            __hits.each do |hit|
+              type = __type(hit)
+              ids_by_type[type] ||= []
+              ids_by_type[type] << hit[:_id]
+            end
+            ids_by_type
+          end
+
+          # Returns the class of the model associated to a certain hit
+          #
+          # A simple class-level memoization over the `_index` and `_type` properties of the hit is applied.
+          # Hence querying the Model Registry is done the minimal amount of times.
+          #
+          # @see Elasticsearch::Model::Registry
+          #
+          # @return Class
+          #
+          # @api private
+          #
+          def __type(hit)
+            @@__types ||= {}
+            @@__types[[hit[:_index], hit[:_type]].join("::")] ||= begin
+              Registry.all.detect { |model| model.index_name == hit[:_index] && model.document_type == hit[:_type] }
+            end
+          end
+
+
+          # Memoizes and returns the hits from the response
+          #
+          # @api private
+          #
+          def __hits
+            @__hits ||= response.response["hits"]["hits"]
+          end
+        end
+      end
+    end
+  end
+end
---
+++ elasticsearch-model/test/unit/adapter_multiple_test.rb
@@ -0,0 +1,106 @@
+require 'test_helper'
+
+class Elasticsearch::Model::MultipleTest < Test::Unit::TestCase
+
+  context "Adapter multiple module" do
+
+    class ::DummyOne
+      include Elasticsearch::Model
+
+      index_name 'dummy'
+      document_type 'dummy_one'
+
+      def self.find(ids)
+        ids.map { |id| new(id) }
+      end
+
+      attr_reader :id
+
+      def initialize(id)
+        @id = id.to_i
+      end
+    end
+
+    module ::Namespace
+      class DummyTwo
+        include Elasticsearch::Model
+
+        index_name 'dummy'
+        document_type 'dummy_two'
+
+        def self.find(ids)
+          ids.map { |id| new(id) }
+        end
+
+        attr_reader :id
+
+        def initialize(id)
+          @id = id.to_i
+        end
+      end
+    end
+
+    class ::DummyTwo
+      include Elasticsearch::Model
+
+      index_name 'other_index'
+      document_type 'dummy_two'
+
+      def self.find(ids)
+        ids.map { |id| new(id) }
+      end
+
+      attr_reader :id
+
+      def initialize(id)
+        @id = id.to_i
+      end
+    end
+
+    HITS = [{_index: 'dummy',
+             _type: 'dummy_two',
+             _id: '2',
+            }, {
+              _index: 'dummy',
+              _type: 'dummy_one',
+              _id: '2',
+            }, {
+              _index: 'other_index',
+              _type: 'dummy_two',
+              _id: '1',
+            }, {
+              _index: 'dummy',
+              _type: 'dummy_two',
+              _id: '1',
+            }, {
+              _index: 'dummy',
+              _type: 'dummy_one',
+              _id: '3'}]
+
+    setup do
+      @multimodel = Elasticsearch::Model::Multimodel.new(DummyOne, DummyTwo, Namespace::DummyTwo)
+    end
+
+    context "Records" do
+      setup do
+        @multimodel.class.send :include, Elasticsearch::Model::Adapter::Multiple::Records
+        @multimodel.expects(:__hits).at_least_once.returns(HITS)
+      end
+
+      should "keep global order among models" do
+        assert_instance_of Module, Elasticsearch::Model::Adapter::Multiple::Records
+        records = @multimodel.records
+
+        assert_equal 5, records.count
+
+        assert_kind_of ::Namespace::DummyTwo, records[0]
+        assert_kind_of ::DummyOne,            records[1]
+        assert_kind_of ::DummyTwo,            records[2]
+        assert_kind_of ::Namespace::DummyTwo, records[3]
+        assert_kind_of ::DummyOne,            records[4]
+
+        assert_equal [2, 2, 1, 1, 3], records.map(&:id)
+      end
+    end
+  end
+end
