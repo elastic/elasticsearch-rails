2b338c1a4454bfb8b3915a297b2d2a7f2148efbe
Miguel Fernandez <miguelfernandezfernandez@gmail.com>
2015-04-11 13:58:17 +0200

    [MODEL] [STORE] Fixed the problem where `document_type` configuration was not propagated to mapping
    This PR tries to fix the bug described in #270
    
    The problem was the following:
    
    * Once a model extends Persistence::Model it sets up two attributes. As a consequence, the attribute macro instantiates a Elasticsearch::Model::Indexing::Mappings in the model. This instance contains an attribute @type that defaults to the model name in lowercase.
    
    * Any other definition of document_type will override the document_type in the gateway delegate, but the mappings instance originally created in the previous step remains untouched, and hence in an inconsistent state.
    
    What I did here is: Instead of simply forward document_type to the gateway, also change the mappings' document_type accordingly, leaving it in a correct state.
    
    An integration test was modified, in order to have a regression of the fix.
    
    Unit tests were added.
    
    Fixes #270
    Closes #366

--- elasticsearch-persistence/test/unit/model_base_test.rb
+++ elasticsearch-persistence/test/unit/model_base_test.rb
@@ -20,29 +20,53 @@ class Elasticsearch::Persistence::ModelBaseTest < Test::Unit::TestCase
     end
 
     should "set the ID from attributes during initialization" do
-      m = DummyBaseModel.new id: 1
-      assert_equal 1, m.id
+      model = DummyBaseModel.new id: 1
+      assert_equal 1, model.id
 
-      m = DummyBaseModel.new 'id' => 2
-      assert_equal 2, m.id
+      model = DummyBaseModel.new 'id' => 2
+      assert_equal 2, model.id
     end
 
     should "set the ID using setter method" do
-      m = DummyBaseModel.new id: 1
-      assert_equal 1, m.id
+      model = DummyBaseModel.new id: 1
+      assert_equal 1, model.id
 
-      m.id = 2
-      assert_equal 2, m.id
+      model.id = 2
+      assert_equal 2, model.id
     end
 
     should "have ID in attributes" do
-      m = DummyBaseModel.new id: 1, name: 'Test'
-      assert_equal 1, m.attributes[:id]
+      model = DummyBaseModel.new id: 1, name: 'Test'
+      assert_equal 1, model.attributes[:id]
     end
 
     should "have the customized inspect method" do
-      m = DummyBaseModel.new name: 'Test'
-      assert_match /name\: "Test"/, m.inspect
+      model = DummyBaseModel.new name: 'Test'
+      assert_match /name\: "Test"/, model.inspect
+    end
+
+    context "with custom document_type" do
+      setup do
+        @model = DummyBaseModel
+        @gateway = mock()
+        @mapping = mock()
+        @model.stubs(:gateway).returns(@gateway)
+        @gateway.stubs(:mapping).returns(@mapping)
+        @document_type = 'dummybase'
+      end
+
+      should "forward the argument to mapping" do
+        @gateway.expects(:document_type).with(@document_type).once
+        @mapping.expects(:type=).with(@document_type).once
+        @model.document_type @document_type
+      end
+
+      should "return the value from the gateway" do
+        @gateway.expects(:document_type).once.returns(@document_type)
+        @mapping.expects(:type=).never
+        returned_type = @model.document_type
+        assert_equal @document_type, returned_type
+      end
     end
   end
 end
