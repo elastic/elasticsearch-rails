9ec745499f9092fb3aa5923d8be215e7d11e481b
Karel Minarik <karel.minarik@elasticsearch.com>
2017-01-28 09:14:54 +0100

    [MODEL] Fixed the failing integration tests for ActiveRecord associations

--- elasticsearch-model/test/integration/active_record_associations_test.rb
+++ elasticsearch-model/test/integration/active_record_associations_test.rb
@@ -115,6 +115,8 @@ module Elasticsearch
           class Author < ActiveRecord::Base
             has_many :authorships
 
+            after_update { self.authorships.each(&:touch) }
+
             def full_name
               [first_name, last_name].compact.join(' ')
             end
@@ -133,8 +135,13 @@ module Elasticsearch
             has_and_belongs_to_many :categories, after_add:    [ lambda { |a,c| a.__elasticsearch__.index_document } ],
                                                  after_remove: [ lambda { |a,c| a.__elasticsearch__.index_document } ]
             has_many                :authorships
-            has_many                :authors, through: :authorships
-            has_many                :comments
+            has_many                :authors, through: :authorships,
+                                              after_add: [ lambda { |a,c| a.__elasticsearch__.index_document } ],
+                                              after_remove: [ lambda { |a,c| a.__elasticsearch__.index_document } ]
+            has_many                :comments, after_add:    [ lambda { |a,c| a.__elasticsearch__.index_document } ],
+                                               after_remove: [ lambda { |a,c| a.__elasticsearch__.index_document } ]
+
+            after_touch() { __elasticsearch__.index_document }
           end
 
           # Include the search integration
