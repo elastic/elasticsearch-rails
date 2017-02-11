c25aa447274e1afcacc243ceb00d229524a2208b
Koen Punt <koen@koenpunt.nl>
2017-02-11 16:45:47 +0100

    use delete_document and pass on options
    the perform method accepted an options hash, but this hash wasn't passed on to the API method.
    also, it was unclear to me why not to use the more high level delete_document method instead of the delete method of the client, so I've updated that as well.

--- elasticsearch-rails/lib/rails/templates/indexer.rb
+++ elasticsearch-rails/lib/rails/templates/indexer.rb
@@ -11,16 +11,14 @@ class Indexer
   Logger = Sidekiq.logger.level == Logger::DEBUG ? Sidekiq.logger : nil
   Client = Elasticsearch::Client.new host: (ENV['ELASTICSEARCH_URL'] || 'http://localhost:9200'), logger: Logger
 
-  def perform(operation, klass, record_id, options={})
+  def perform(operation, klass, record_id, options = {})
     logger.debug [operation, "#{klass}##{record_id} #{options.inspect}"]
 
     case operation.to_s
-      when /index|update/
+      when /index|update|delete/
         record = klass.constantize.find(record_id)
         record.__elasticsearch__.client = Client
-        record.__elasticsearch__.__send__ "#{operation}_document"
-      when /delete/
-        Client.delete index: klass.constantize.index_name, type: klass.constantize.document_type, id: record_id
+        record.__elasticsearch__.__send__ "#{operation}_document", options
       else raise ArgumentError, "Unknown operation '#{operation}'"
     end
   end
