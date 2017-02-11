60534a9c77b50c2262ba1516f8c3dcab103b8276
Koen Punt <koen@koenpunt.nl>
2017-02-11 17:01:07 +0100

    use a guard instead of a conditional block

--- elasticsearch-rails/lib/rails/templates/indexer.rb
+++ elasticsearch-rails/lib/rails/templates/indexer.rb
@@ -14,12 +14,10 @@ class Indexer
   def perform(operation, klass, record_id, options = {})
     logger.debug [operation, "#{klass}##{record_id} #{options.inspect}"]
 
-    case operation.to_s
-      when /index|update|delete/
-        record = klass.constantize.find(record_id)
-        record.__elasticsearch__.client = Client
-        record.__elasticsearch__.__send__ "#{operation}_document", options
-      else raise ArgumentError, "Unknown operation '#{operation}'"
-    end
+    raise ArgumentError, "Unknown operation '#{operation}'" unless %w(index update delete).include?(operation.to_s)
+
+    record = klass.constantize.find(record_id)
+    record.__elasticsearch__.client = Client
+    record.__elasticsearch__.__send__ "#{operation}_document", options
   end
 end
