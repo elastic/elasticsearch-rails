109c155df6c8b8989aa935bc1fe163a1452a2b4e
Karel Minarik <karel.minarik@elasticsearch.com>
2017-03-31 15:00:15 +0200

    [MODEL] Updated the "completion suggester" example
    Let's use an embedded field instead of a suffixed property for the `suggest` document property.
    
    Also added an example of search with suggest, including source filtering.
    
    Related: #690

--- elasticsearch-model/examples/activerecord_mapping_completion.rb
+++ elasticsearch-model/examples/activerecord_mapping_completion.rb
@@ -18,8 +18,9 @@ class Article < ActiveRecord::Base
   include Elasticsearch::Model::Callbacks
 
   mapping do
-    indexes :title, type: 'text'
-    indexes :title_suggest, type: 'completion'
+    indexes :title, type: 'text' do
+      indexes :suggest, type: 'completion'
+    end
     indexes :url, type: 'keyword'
   end
 
@@ -56,12 +57,28 @@ response_2 = Article.__elasticsearch__.client.suggest \
   body: {
     articles: {
       text: 'foo',
-      completion: { field: 'title_suggest', size: 25 }
+      completion: { field: 'title.suggest' }
     }
   };
 
 puts "Article suggest:".ansi(:bold),
      response_2['articles'].first['options'].map { |d| "#{d['text']} -> #{d['_source']['url']}" }.
      inspect.ansi(:bold, :green)
+
+response_3 = Article.search \
+  query: {
+    match: { title: 'foo' }
+  },
+  suggest: {
+    articles: {
+      text: 'foo',
+      completion: { field: 'title.suggest' }
+    }
+  },
+  _source: ['title', 'url']
+
+puts "Article search with suggest:".ansi(:bold),
+     response_3.response['suggest']['articles'].first['options'].map { |d| "#{d['text']} -> #{d['_source']['url']}" }.
+     inspect.ansi(:bold, :blue)
 
 require 'pry'; binding.pry;
