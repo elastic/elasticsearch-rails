ca76ab6c538ed1b44e1a15a02828cd70953d0eae
Michael Holroyd <meekohi@gmail.com>
2017-04-03 10:47:08 -0400

    Check object is persisted before pushing to ES.
    Ensure Article is actually saved (as opposed to just having an id assigned) before pushing to Elasticsearch.

--- elasticsearch-model/examples/activerecord_associations.rb
+++ elasticsearch-model/examples/activerecord_associations.rb
@@ -119,8 +119,8 @@ end
 class Article < ActiveRecord::Base
   include Searchable
 
-  has_and_belongs_to_many :categories, after_add:    [ lambda { |a,c| a.__elasticsearch__.index_document if a.id } ],
-                                       after_remove: [ lambda { |a,c| a.__elasticsearch__.index_document if a.id } ]
+  has_and_belongs_to_many :categories, after_add:    [ lambda { |a,c| a.__elasticsearch__.index_document if a.persisted? } ],
+                                       after_remove: [ lambda { |a,c| a.__elasticsearch__.index_document if a.persisted? } ]
   has_many                :authorships
   has_many                :authors, through: :authorships
   has_many                :comments
