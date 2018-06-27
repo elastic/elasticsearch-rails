719026cbcdfb9c34ed8a6ff70419c6d6cae0d755
Emily Stolfo <emstolfo@gmail.com>
2018-06-27 12:35:11 +0200

    Update versions to 6.0.0.beta

--- elasticsearch-persistence/lib/elasticsearch/persistence/version.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/version.rb
@@ -1,5 +1,5 @@
 module Elasticsearch
   module Persistence
-    VERSION = "6.0.0.alpha1"
+    VERSION = "6.0.0.beta"
   end
 end
--- elasticsearch-rails/lib/elasticsearch/rails/version.rb
+++ elasticsearch-rails/lib/elasticsearch/rails/version.rb
@@ -1,5 +1,5 @@
 module Elasticsearch
   module Rails
-    VERSION = "6.0.0.alpha1"
+    VERSION = "6.0.0.beta"
   end
 end
