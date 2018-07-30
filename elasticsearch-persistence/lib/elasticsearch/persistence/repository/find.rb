2110bc3046bea70658876821bfadad117dc9ce9c
Emily S <emily.s@elastic.co>
2018-07-30 15:56:18 +0200

    [STORE] Deprecate _all field in ES 6.x (#820)

--- elasticsearch-persistence/lib/elasticsearch/persistence/repository/find.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/repository/find.rb
@@ -7,10 +7,6 @@ module Elasticsearch
       #
       module Find
 
-        # The default type of document.
-        #
-        ALL = '_all'.freeze
-
         # The key for accessing the document found and returned from an
         #   Elasticsearch _mget query.
         #
@@ -57,16 +53,17 @@ module Elasticsearch
         # @return [true, false]
         #
         def exists?(id, options={})
-          type     = document_type || ALL
-          client.exists( { index: index_name, type: type, id: id }.merge(options) )
+          request = { index: index_name, id: id }
+          request[:type] = document_type if document_type
+          client.exists(request.merge(options))
         end
 
         # @api private
         #
         def __find_one(id, options={})
-          type     = document_type || ALL
-          document = client.get( { index: index_name, type: type, id: id }.merge(options) )
-
+          request = { index: index_name, id: id }
+          request[:type] = document_type if document_type
+          document = client.get(request.merge(options))
           deserialize(document)
         rescue Elasticsearch::Transport::Transport::Errors::NotFound => e
           raise DocumentNotFound, e.message, caller
@@ -75,13 +72,12 @@ module Elasticsearch
         # @api private
         #
         def __find_many(ids, options={})
-          type     = document_type || ALL
-          documents = client.mget( { index: index_name, type: type, body: { ids: ids } }.merge(options) )
-
+          request = { index: index_name, body: { ids: ids } }
+          request[:type] = document_type if document_type
+          documents = client.mget(request.merge(options))
           documents[DOCS].map { |document| document[FOUND] ? deserialize(document) : nil }
         end
       end
-
     end
   end
 end
