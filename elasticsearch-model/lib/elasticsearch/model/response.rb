685845a10ed306f7f6282ac7990c83371ea305a8
Matt Coneybeare <matt@coneybeare.me>
2018-10-05 14:46:50 -0400

    Prevent double calls to search.execute! by using optional cached bollean to return raw_response

--- elasticsearch-model/lib/elasticsearch/model/response.rb
+++ elasticsearch-model/lib/elasticsearch/model/response.rb
@@ -23,10 +23,13 @@ module Elasticsearch
 
         # Returns the Elasticsearch response
         #
+        # @param cached [Boolean] Optional parameter that returns the cached initial call
+        #                         instead of executing a new search every time
+        #
         # @return [Hash]
         #
-        def response
-          @response ||= HashWrapper.new(search.execute!)
+        def response(cached = false)
+          @response ||= HashWrapper.new(cached ? raw_results : search.execute!)
         end
 
         # Returns the collection of "hits" from Elasticsearch
