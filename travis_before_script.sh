e5d8bf926718df0177952496ae17096fffdb5c74
Emily Stolfo <emstolfo@gmail.com>
2019-03-27 17:25:03 +0100

    Fix Travis before script

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-metadata_url="https://artifacts-api.elastic.co/v1/branches/master/builds/latest/projects/elasticsearch/packages/elasticsearch-${ELASTICSEARCH_VERSION}-SNAPSHOT.zip/file"
-echo "Getting snapshot location from $metadata_url"
-
-url=$(curl -v $metadata_url 2>&1 | grep -Pio 'location: \K(.*)' | tr -d '\r')
-
-echo "Downloading Elasticsearch from $url"
-curl $url -o /tmp/elasticsearch.zip
-
-echo 'Unzipping file'
-unzip -q /tmp/elasticsearch.zip
+curl https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}-linux-x86_64.tar.gz | tar xz -C /tmp
 
 echo "Starting elasticsearch on port ${TEST_CLUSTER_PORT}"
-${PWD}/elasticsearch-${ELASTICSEARCH_VERSION}-SNAPSHOT/bin/elasticsearch -E http.port=${TEST_CLUSTER_PORT} &> /dev/null &
+/tmp/elasticsearch-${ELASTICSEARCH_VERSION}/bin/elasticsearch -E http.port=${TEST_CLUSTER_PORT} &> /dev/null &
