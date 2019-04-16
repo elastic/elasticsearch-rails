1be9c7e31310b42a2e33a980ca3a1d67b6585151
Emily Stolfo <emstolfo@gmail.com>
2019-04-16 12:07:38 +0200

    Update script to download and start elasticsearch

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-curl https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}-linux-x86_64.tar.gz | tar xz -C /tmp
+if [ "$ELASTICSEARCH_VERSION" == "6.7.1" ]
+then
+    url="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz"
+else
+    url="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}-linux-x86_64.tar.gz"
+fi
+
+echo "Downloading elasticsearch from $url"
+curl $url | tar xz -C /tmp
 
 echo "Starting elasticsearch on port ${TEST_CLUSTER_PORT}"
+/tmp/elasticsearch-${ELASTICSEARCH_VERSION}/bin/elasticsearch-keystore create
 /tmp/elasticsearch-${ELASTICSEARCH_VERSION}/bin/elasticsearch -E http.port=${TEST_CLUSTER_PORT} &> /dev/null &
