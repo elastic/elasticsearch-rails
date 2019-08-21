00e889d29cae28db88d333d68f458be2728e4e0b
Emily Stolfo <emstolfo@gmail.com>
2019-08-21 15:54:15 +0200

    Release 7.0.0

--- elasticsearch-model/lib/elasticsearch/model/version.rb
+++ elasticsearch-model/lib/elasticsearch/model/version.rb
@@ -17,6 +17,6 @@
 
 module Elasticsearch
   module Model
-    VERSION = "7.0.0.pre"
+    VERSION = "7.0.0"
   end
 end
--- elasticsearch-persistence/lib/elasticsearch/persistence/version.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/version.rb
@@ -17,6 +17,6 @@
 
 module Elasticsearch
   module Persistence
-    VERSION = '7.0.0.pre'
+    VERSION = '7.0.0'
   end
 end
