6633effe7ac1b7fb6aec48242ed2d6e90ad03169
Sean Floyd <SeanLF@users.noreply.github.com>
2021-04-09 12:05:03 +0200

    Fix for ruby < 2.7

--- elasticsearch-model/lib/elasticsearch/model/adapters/multiple.rb
+++ elasticsearch-model/lib/elasticsearch/model/adapters/multiple.rb
@@ -73,7 +73,7 @@ module Elasticsearch
             # Allow calling `.records()` with options:
             # klass.name => [{ method: :includes, args: [:association]}, { method: :scope_name }]
             if (klass_options = @options.dig(klass.name))
-              klass_options.each { klass = klass.public_send(_1[:method], *_1[:args]) }
+              klass_options.each { |opts| klass = klass.public_send(opts[:method], *opts[:args]) }
             end
 
             case
