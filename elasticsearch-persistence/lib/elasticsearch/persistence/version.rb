a7455270d10e59cd092419a9473481d5b23235e4
Fernando Briano <fernando@picandocodigo.net>
2021-08-11 10:45:01 +0100

    Bumps version to 7.2.0

--- elasticsearch-persistence/lib/elasticsearch/persistence/version.rb
+++ elasticsearch-persistence/lib/elasticsearch/persistence/version.rb
@@ -17,6 +17,6 @@
 
 module Elasticsearch
   module Persistence
-    VERSION = '7.1.0'
+    VERSION = '7.2.0'
   end
 end
--- elasticsearch-rails/lib/elasticsearch/rails/version.rb
+++ elasticsearch-rails/lib/elasticsearch/rails/version.rb
@@ -17,6 +17,6 @@
 
 module Elasticsearch
   module Rails
-    VERSION = "7.1.0"
+    VERSION = "7.2.0"
   end
 end
